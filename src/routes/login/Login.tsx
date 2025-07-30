12334a1808d5302fe02af1be74ac583e404ea7ee
José Henrique <zzaa111529@gmail.com>
2025-07-30 13:21:24 -0300

    Implementando comentários da função de cada código para pasta 'login'

--- src/routes/login/Login.tsx
+++ src/routes/login/Login.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import './Login.css';
 import logo from '../../assets/logo.svg';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
+// Componente de Login: Renderiza a página de autenticação do usuário.
 const Login: React.FC = () => {
+  // Lógica de manipulação do envio do formulário de login.
   const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log('Tentativa de login...');
   };
 
   return (
+    // Contêiner principal que centraliza o conteúdo da página.
     <div className="login-page">
       <div className="login-container">
-       
-        <div className="login-logo-placeholder">
-            <img src={logo} alt="logo" />
-        </div>
 
+        <div className="login-logo-placeholder">
+          <img src={logo} alt="logo" />
+        </div>
 
         <form onSubmit={handleLogin} className="login-form">
           <div className="input-group">
@@ -40,7 +42,7 @@ const Login: React.FC = () => {
 
           <Link to="/home">
             <button type="submit" className="login-button">
-                ENTRAR
+              ENTRAR
             </button>
           </Link>
 
