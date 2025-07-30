b60db19b555df687297664d270d28c0aa91e6358
José Henrique <zzaa111529@gmail.com>
2025-07-30 20:52:05 -0300

    Implementando comentários da função de cada código para pasta 'navbar'

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,20 +1,24 @@
 import './Navbar.css';
 import avatar from '../../assets/avatar.svg'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Navbar = ({sidebarOpen, openSidebar}) => {
+// Componente da barra de navegação superior (Navbar).
+const Navbar = ({ openSidebar }) => {
     return (
+        // Container principal da barra de navegação.
         <nav className="navbar">
+            {/* Ícone do menu (hambúrguer) para abrir a sidebar em telas menores. */}
             <div className="nav_icon" onClick={() => openSidebar()}>
                 <i className="bi bi-list" aria-hidden="true"></i>
             </div>
 
+            {/* Seção esquerda da navbar, para links de navegação (atualmente comentados). */}
             <div className="navbar_left">
                 {/* <a href="#">Produtos</a>
                 <a href="#">Usuários</a>
                 <a href="#" className="active_link">Admin</a> */}
             </div>
 
+            {/* Seção direita da navbar, para ícone do perfil do usuário. */}
             <div className="navbar_right">
                 <a href="#">
                     <img width="30" src={avatar} alt="avatar" />
