7a307887c379ec0fd32c0a8a939f1c6ef43e608c
José Henrique <zzaa111529@gmail.com>
2025-07-30 21:08:05 -0300

    Implementando Menu do Usuário ao clicar na Logo de Avatar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
 import './Navbar.css';
-import avatar from '../../assets/avatar.svg'
+import avatar from '../../assets/avatar.svg';
+import {Link} from 'react-router-dom';
 
-// Componente da barra de navegação superior (Navbar).
 const Navbar = ({ openSidebar }) => {
+    // Estado para controlar a visibilidade do menu dropdown
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    // Função para alternar a visibilidade do menu
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    // Função para obter a saudação correta com base na hora
+    const getGreeting = () => {
+        const hour = new Date().getHours();
+        if (hour < 12) {
+            return "Bom Dia";
+        } else if (hour < 18) {
+            return "Boa Tarde";
+        } else {
+            return "Boa Noite";
+        }
+    };
+
     return (
         // Container principal da barra de navegação.
         <nav className="navbar">
@@ -11,18 +32,25 @@ const Navbar = ({ openSidebar }) => {
                 <i className="bi bi-list" aria-hidden="true"></i>
             </div>
 
-            {/* Seção esquerda da navbar, para links de navegação (atualmente comentados). */}
+            {/* Seção esquerda da navbar com saudação dinâmica */}
             <div className="navbar_left">
-                {/* <a href="#">Produtos</a>
-                <a href="#">Usuários</a>
-                <a href="#" className="active_link">Admin</a> */}
+                <span>{getGreeting()}, Admin!</span>
             </div>
 
-            {/* Seção direita da navbar, para ícone do perfil do usuário. */}
+            {/* Seção direita da navbar, com avatar e menu dropdown. */}
             <div className="navbar_right">
-                <a href="#">
+                <a href="#!" onClick={toggleMenu}>
                     <img width="30" src={avatar} alt="avatar" />
                 </a>
+
+                {/* Renderiza o menu dropdown se isMenuOpen for true */}
+                {isMenuOpen && (
+                    <div className="dropdown_menu">
+                        <a href="#!">Editar Perfil</a>
+                        <a href="#!">Configurações</a>
+                        <Link to="/">SAIR</Link>
+                    </div>
+                )}
             </div>
         </nav>
     )
