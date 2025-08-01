f9e94f0af5683efe6d59da50d3d0b5fb135dc361
José Henrique <zzaa111529@gmail.com>
2025-07-31 21:22:33 -0300

    Melhorando painel de Dashboard

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -17,34 +17,34 @@ const Main = () => {
                 {/* Seção que agrupa os cards de resumo (KPIs). */}
                 <div className="main_cards">
                     <div className="card main_cards_1">
-                        <span className="text-title main_card_1">578</span>
+                        <span className="text-title main_card_1">350</span>
                         <div className="card_inner">
                             <p className="text-primary-p">Clientes</p>
-                            <i className="bi bi-currency-dollar text-lightblue big_icon"></i>
+                            <i className="bi bi-person-fill main_card_1 big_icon"></i>
                         </div>
                     </div>
 
                     <div className="card main_cards_2">
                         <span className="text-title main_card_2">578</span>
                         <div className="card_inner">
-                            <p className="text-primary-p">Clientes</p>
-                            <i className="bi bi-currency-dollar text-lightblue big_icon"></i>
+                            <p className="text-primary-p">Prestadores</p>
+                            <i className="bi bi-person-fill-gear main_card_2 big_icon"></i>
                         </div>
                     </div>
 
                     <div className="card main_cards_3">
-                        <span className="text-title main_card_3">578</span>
+                        <span className="text-title main_card_3">100</span>
                         <div className="card_inner">
-                            <p className="text-primary-p">Clientes</p>
-                            <i className="bi bi-currency-dollar text-lightblue big_icon"></i>
+                            <p className="text-primary-p">Agendamentos</p>
+                            <i className="bi bi-calendar-check-fill main_card_3 big_icon"></i>
                         </div>
                     </div>
 
                     <div className="card main_cards_4">
-                        <span className="text-title main_card_4">578</span>
+                        <span className="text-title main_card_4">40</span>
                         <div className="card_inner">
-                            <p className="text-primary-p">Clientes</p>
-                            <i className="bi bi-currency-dollar text-lightblue big_icon"></i>
+                            <p className="text-primary-p">Chamados</p>
+                            <i className="bi bi-headset main_card_4 big_icon"></i>
                         </div>
                     </div>
                 </div>
@@ -54,8 +54,8 @@ const Main = () => {
                     <div className="charts_left">
                         <div className="charts_left_title">
                             <div>
-                                <h1>Daily Reports</h1>
-                                <p>Ubatuba, São Paulo, BR</p>
+                                <h1>Faturamento</h1>
+                                <p>Sombrio, sc</p>
                             </div>
                             <i className="fa fa-usd"></i>
                         </div>
@@ -65,31 +65,31 @@ const Main = () => {
                     <div className="charts_right">
                         <div className="charts_right_title">
                             <div>
-                                <h1>Daily Reports</h1>
-                                <p>Ubatuba, São Paulo, BR</p>
+                                <h1>Brasil</h1>
+                                <p>Geral</p>
                             </div>
                             <i className="fa fa-area-chart"></i>
                         </div>
 
                         <div className="charts_right_cards">
                             <div className="card1">
-                                <h1>Lucro</h1>
-                                <p>R$2500</p>
+                                <h1>Serviços</h1>
+                                <p>+300</p>
                             </div>
 
                             <div className="card2">
                                 <h1>Lucro</h1>
-                                <p>R$2500</p>
+                                <p>R$20.500</p>
                             </div>
 
                             <div className="card3">
-                                <h1>Lucro</h1>
-                                <p>R$2500</p>
+                                <h1>Clientes</h1>
+                                <p>+400</p>
                             </div>
 
                             <div className="card4">
-                                <h1>Lucro</h1>
-                                <p>R$2500</p>
+                                <h1>Prestadores</h1>
+                                <p>+900</p>
                             </div>
                         </div>
                     </div>
